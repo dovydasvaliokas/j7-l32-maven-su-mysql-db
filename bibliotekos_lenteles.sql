efedb74a67d04464a0218951c039486236c61074
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-21 12:50:50 +0300

    Sukurtas maven projektas bei sukurtos sql lenteles ir issaugotos faile

---
+++ bibliotekos_lenteles.sql
@@ -0,0 +1,30 @@
+-- Knyga lentelė
+CREATE TABLE IF NOT EXISTS knyga (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    pavadinimas VARCHAR(50) NOT NULL,
+    aprasymas VARCHAR(300),
+    kaina DECIMAL(5,2) UNSIGNED NOT NULL,
+    puslapiu_skaicius INT UNSIGNED NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE=INNODB;
+
+
+-- klientai lentelė
+CREATE TABLE IF NOT EXISTS klientai (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    vardas VARCHAR(50) NOT NULL,
+    pavarde VARCHAR(50) NOT NULL,
+    login VARCHAR(30) NOT NULL,
+    slaptazodis VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+) ENGINE = INNODB;
+    
+
+-- autoriai
+CREATE TABLE IF NOT EXISTS autoriai (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    vardas VARCHAR(50) NOT NULL,
+    pavarde VARCHAR(50) NOT NULL,
+    šalis VARCHAR(50),
+    PRIMARY KEY(id)
+) ENGINE = INNODB;
