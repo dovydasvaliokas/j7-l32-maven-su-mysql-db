ac25b32dca4a31cbcb40ca95d0d4580cc882bbe7
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-07-25 12:31:26 +0300

    Isbandau nuskaitymo is konsoles funkcija

--- src/main/java/utility/PapildomiVeiksmai.java
+++ src/main/java/utility/PapildomiVeiksmai.java
@@ -1,6 +1,7 @@
 package utility;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import entity.Knyga;
 
@@ -12,5 +13,25 @@ public class PapildomiVeiksmai {
         for (Knyga knyga : knygos) {
             System.out.println("knyga = " + knyga);
         }
+    }
+
+
+    public static Knyga nuskaitytiKnygaIsKonsoles() {
+        Scanner skait = new Scanner(System.in);
+        System.out.println("Įveskite knygos pavadinimą: ");
+        String pavadinimas = skait.nextLine();
+        System.out.println("Įveskite knygos aprašymą: ");
+        String aprasymas = skait.nextLine();
+        System.out.println("Įveskite knygos kainą: ");
+        double kaina = skait.nextDouble();
+        System.out.println("Įveskite knygos puslapių skaičių: ");
+        int pslSk = skait.nextInt();
+
+        System.out.println("pslSk = " + pslSk);
+        System.out.println("kaina = " + kaina);
+        System.out.println("aprasymas = " + aprasymas);
+        System.out.println("pavadinimas = " + pavadinimas);
+
+        return new Knyga();
     }
 }
